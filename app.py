0c6c9923c76c191ebfccd68872dcb53c1533d7fc
Chris <cqolee@ucdavis.edu>
2024-09-11 21:14:58 -0700

    Fix port binding issue

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def github_webhook():
 if __name__ == "__main__":
     # send_slack_notifications("hi")
     port = int(os.environ.get('PORT', 5000))  # Use the 'PORT' environment variable if set, default to 5000
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 
